b6e6fa118052bd4ac7549e4a660f546d724d43a6
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-05-03 00:00:49 +0300

    Add try/catch to API.js; Add getTrending to Homepage's useEffect

--- src/API/API.js
+++ src/API/API.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
 const API_KEY = "635ac74a11f3f55a21a984cd556f5915";
 
@@ -8,7 +7,7 @@ const getTrendingFilms = async () => {
   try {
     const data = await axios.get(`trending/all/day?api_key=${API_KEY}`);
     console.log(data.data.results);
-    return data;
+    return data.data.results;
   } catch (error) {
     console.error(error);
   }
--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,6 +1,21 @@
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { getTrendingFilms } from "../../API/API";
 
 const Homepage = () => {
+  const [trendingsFilms, setTrendingsFilms] = useState([]);
+
+  useEffect(() => {
+    const getTrending = async () => {
+      try {
+        const data = await getTrendingFilms();
+        setTrendingsFilms(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getTrending();
+  }, []);
   return (
     <>
       <h1>This is a Homepage</h1>
