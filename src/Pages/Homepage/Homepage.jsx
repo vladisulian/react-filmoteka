0366df306e14b9eb1996b94a46ba75b26a196324
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-05-03 00:10:16 +0300

    Edit homepage.jsx

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,11 +1,15 @@
-import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import { getTrendingFilms } from "../../API/API";
+import TrendingsList from "../../Components/Smart/TrendingsList/TrendingsList";
 
 const Homepage = () => {
   const [trendingsFilms, setTrendingsFilms] = useState([]);
 
+  // get trending films
   useEffect(() => {
+    // Создал новую асинхронную функцию, потому что хуки всегда синхронные.
+    // Здесь было 2 решения: либо промисы, либо новая функция и await с try/catch
+
     const getTrending = async () => {
       try {
         const data = await getTrendingFilms();
@@ -16,9 +20,10 @@ const Homepage = () => {
     };
     getTrending();
   }, []);
+
   return (
     <>
-      <h1>This is a Homepage</h1>
+      <TrendingsList trendingsFilms={trendingsFilms} />
     </>
   );
 };
