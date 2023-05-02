0a1d0b56d3d412df0823c020d122ef8e52800a11
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-05-02 23:49:51 +0300

    Add getTrendingFilms function

---
+++ src/API/API.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+const API_KEY = "635ac74a11f3f55a21a984cd556f5915";
+
+// https://api.themoviedb.org/3/trending/all/day?api_key=635ac74a11f3f55a21a984cd556f5915
+const getTrendingFilms = async () => {
+  try {
+    const data = await axios.get(`trending/all/day?api_key=${API_KEY}`);
+    console.log(data.data.results);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getTrendingFilms };
