447ed3217784b418e446e31f4bc77a05de38ab62
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-05-03 10:57:19 +0300

    Add getPosterPath function

--- src/API/API.js
+++ src/API/API.js
@@ -3,6 +3,11 @@ axios.defaults.baseURL = "https://api.themoviedb.org/3";
 const API_KEY = "635ac74a11f3f55a21a984cd556f5915";
 
 // https://api.themoviedb.org/3/trending/all/day?api_key=635ac74a11f3f55a21a984cd556f5915
+
+const getPosterPath = (posterPath) => {
+  return `https://image.tmdb.org/t/p/original/${posterPath}`;
+};
+
 const getTrendingFilms = async () => {
   try {
     const data = await axios.get(`trending/all/day?api_key=${API_KEY}`);
@@ -13,4 +18,4 @@ const getTrendingFilms = async () => {
   }
 };
 
-export { getTrendingFilms };
+export { getTrendingFilms, getPosterPath };
