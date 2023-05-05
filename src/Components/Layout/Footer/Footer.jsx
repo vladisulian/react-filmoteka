08400246bc136865c427c31547e6d843dea0c91a
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-05-05 18:23:22 +0300

    Add footer

--- src/Components/Layout/Footer/Footer.jsx
+++ src/Components/Layout/Footer/Footer.jsx
@@ -1,9 +1,13 @@
-import("./Footer.css");
+import heartSVG from "./heart.svg";
+import("./Footer.scss");
 
 const Footer = () => {
   return (
     <>
-      <footer>Footer</footer>
+      <footer>
+        <p>© 2020 | All Rights Reserved |</p>
+        <p>Developed with <img src={heartSVG} alt="GoIT heart" /> by GoIT Students</p>
+      </footer>
     </>
   );
 };
