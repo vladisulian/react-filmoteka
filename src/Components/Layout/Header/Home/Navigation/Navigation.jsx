c14659ffb88071af09e1d203277abeb0b58161a8
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-05-05 18:11:13 +0300

    Add dynamic changes in homelink styles (navigation.jsx)

--- src/Components/Layout/Header/Home/Navigation/Navigation.jsx
+++ src/Components/Layout/Header/Home/Navigation/Navigation.jsx
@@ -1,10 +1,26 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import logo from "../../Images/logo.svg";
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useState, useRef } from "react";
 import("./Navigation.scss");
 
 const Navigation = memo(() => {
   const [mobileDevice, setmobileDevice] = useState(true);
+
+  const homeLinkRef = useRef(null);
+
+  const location = useLocation();
+  const onHomepage = location.pathname === "/react-filmoteka";
+
+  useEffect(() => {
+    // Homelink .active class don't work how i want it need to work.
+    // Thats why i create this hook, who handle change homelink class
+
+    //? If we're on homepage - add class .active to homelink
+    //? Else remove it
+    onHomepage
+      ? homeLinkRef.current.classList.add("active")
+      : homeLinkRef.current.classList.remove("active");
+  }, [onHomepage]);
 
   useEffect(() => {
     // function used for dynamic creating Filmoteka title
@@ -28,7 +44,9 @@ const Navigation = memo(() => {
           <img src={logo} className="logo-image" alt="site logo" />
           {!mobileDevice && <p className="logo-text">Filmoteka</p>}
         </Link>
-        <NavLink to={""}>Home</NavLink>
+        <Link to={""} ref={homeLinkRef}>
+          Home
+        </Link>
         <NavLink to={"library"}>Library</NavLink>
       </nav>
     </>
