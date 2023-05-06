5df362ca33d2c755c5eba89c72f2cfafb9ec094b
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-05-06 12:10:14 +0300

    Edit footer

--- src/Components/Layout/Footer/Footer.jsx
+++ src/Components/Layout/Footer/Footer.jsx
@@ -1,15 +1,19 @@
+import { memo } from "react";
 import heartSVG from "./heart.svg";
 import("./Footer.scss");
 
-const Footer = () => {
+const Footer = memo(() => {
   return (
     <>
       <footer>
         <p>© 2020 | All Rights Reserved |</p>
-        <p>Developed with <img src={heartSVG} alt="GoIT heart" /> by GoIT Students</p>
+        <p>
+          Developed with <img src={heartSVG} alt="GoIT heart" /> by GoIT
+          Students
+        </p>
       </footer>
     </>
   );
-};
+});
 
 export default Footer;
